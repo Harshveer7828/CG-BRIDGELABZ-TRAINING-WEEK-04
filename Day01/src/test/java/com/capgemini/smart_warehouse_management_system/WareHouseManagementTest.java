3e6e072f16f7e23e92bb7e6ad377d5595f7cba4b
harshveer7828 <harshveer7828@gmail.com>
2025-02-10 15:17:32 +0530

    [Harsh veer singh] Refactor. WareHouse Management system delete the unneccesary import test file added✨

--- Day01/src/test/java/com/capgemini/smart_warehouse_management_system/WareHouseManagementTest.java
+++ Day01/src/test/java/com/capgemini/smart_warehouse_management_system/WareHouseManagementTest.java
@@ -3,10 +3,8 @@ package com.capgemini.smart_warehouse_management_system;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WareHouseManagementTest {
     Storage<Electronics> electronicsStorage;
