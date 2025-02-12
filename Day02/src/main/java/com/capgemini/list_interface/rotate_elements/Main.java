28f918d99be30c2b8070f0589fd9544d22afe235
harshveer7828 <harshveer7828@gmail.com>
2025-02-12 10:32:30 +0530

    [Harsh veer singh] Refactor. changes in the main file -> rotate the list test file added📦

--- Day02/src/main/java/com/capgemini/list_interface/rotate_elements/Main.java
+++ Day02/src/main/java/com/capgemini/list_interface/rotate_elements/Main.java
@@ -1,17 +1,18 @@
 package com.capgemini.list_interface.rotate_elements;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static com.capgemini.list_interface.rotate_elements.RotateList.rotateList;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(List.of(10, 20, 30, 40, 50));
-        int rotateBy = 2;
+        List<Integer> list = new ArrayList<>(Arrays.asList(10, 20, 30, 40, 50));
+        int position = 2;
 
         System.out.println("Original List: " + list);
-        List<Integer> rotatedList = rotateList(list, rotateBy);
+        List<Integer> rotatedList = rotateList(list, position);
         System.out.println("Rotated List: " + rotatedList);
     }
 }
